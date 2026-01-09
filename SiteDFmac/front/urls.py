29a2ca919e1b1a7ba6f7d47488a09b97a3cd68c3
unknown <nanaromeo237@gmail.com>
2026-01-09 15:49:29 +0100

    indexiation suite

--- SiteDFmac/front/urls.py
+++ SiteDFmac/front/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('condition-generale-de-vente/', cgv, name='cgv'),
     path('politique-de-confidentialite/', get_politique, name='politique'),
     path('domaine-expertise/engenerie-de-projet', get_domaine_expertise, name='domaine.expertise'),
-    path('domaine-expertise/boutique-en-ligne', get_boutique_expertise, name='domaine.boutique'), 
+    path('domaine-expertise/etriers-pour-toiture-et-bardage', get_boutique_expertise, name='domaine.boutique'), 
     path('domaine-expertise/fabrication-d-armatures', get_fa_armature_expertise, name='domaine.faarmature'), 
     path('domaine-expertise/pose-d-armatures', get_pose_armature_expertise, name='domaine.pose.armature'), 
 ]
